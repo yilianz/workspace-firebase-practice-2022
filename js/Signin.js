a9a246f512a205abfc95fd8002235a9e2887d3e4
yilianz <yilianz@gmail.com>
2021-11-15 00:36:06 -0500

    1 file modified, 1 added

--- js/Signin.js
+++ js/Signin.js
@@ -1,30 +1,31 @@
 var firebaseConfig = {
-  apiKey: "AIzaSyAzcwgZuLA7dO9g4sQhXQVTUgCo0M8m2qM",
-  authDomain: "grocerylist-91956.firebaseapp.com",
-  databaseURL: "https://grocerylist-91956.firebaseio.com",
-  projectId: "grocerylist-91956",
-  storageBucket: "grocerylist-91956.appspot.com",
-  messagingSenderId: "813812426276",
-  appId: "1:813812426276:web:93e5897af12892ff78dab1",
-  measurementId: "G-VZ83BTR72T"
+  apiKey: 'AIzaSyAzcwgZuLA7dO9g4sQhXQVTUgCo0M8m2qM',
+  authDomain: 'grocerylist-91956.firebaseapp.com',
+  databaseURL: 'https://grocerylist-91956.firebaseio.com',
+  projectId: 'grocerylist-91956',
+  storageBucket: 'grocerylist-91956.appspot.com',
+  messagingSenderId: '813812426276',
+  appId: '1:813812426276:web:93e5897af12892ff78dab1',
+  measurementId: 'G-VZ83BTR72T',
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
 // save the data
-$("#Login").submit(function(e) {
+$('#Login').submit(function (e) {
   e.preventDefault();
   // get the user name and password from form
-  // You need to change this. 
-  var email = "yilianz4@gmail.com";
-  var password = "ddsgagafda";
+  // You need to change this.
+  var email = 'yilianz4@gmail.com';
+  var password = 'ddsgagafda111';
+
   firebase
     .auth()
     .signInWithEmailAndPassword(email, password)
-    .then(success => {
+    .then((success) => {
       // Signed in
       // ...
-      console.log("login in");
+      console.log('login in');
       let user = firebase.auth().currentUser;
 
       //user.updateProfile({ displayName: "Not sure" });
@@ -35,8 +36,19 @@ $("#Login").submit(function(e) {
         emailVerified = user.emailVerified;
         console.log(name + email + emailVerified);
       }
+      window.location.href = 'game.html';
     })
-    .catch(error => {
+    .catch((error) => {
+      firebase
+        .auth()
+        .signOut()
+        .then(() => {
+          // Sign-out successful.
+        })
+        .catch((error) => {
+          // An error happened.
+        });
+
       var errorCode = error.code;
       var errorMessage = error.message;
       console.log(errorMessage);
