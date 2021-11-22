5fc4d5ce59b484014ed1b0c39ea2c0d86133feda
yilianz <yilianz@gmail.com>
2021-11-22 11:42:44 -0500

    2 files modified

--- js/hotel-json.js
+++ js/hotel-json.js
@@ -1,12 +1,12 @@
 var firebaseConfig = {
-  apiKey: "AIzaSyAzcwgZuLA7dO9g4sQhXQVTUgCo0M8m2qM",
-  authDomain: "grocerylist-91956.firebaseapp.com",
-  databaseURL: "https://grocerylist-91956.firebaseio.com",
-  projectId: "grocerylist-91956",
-  storageBucket: "grocerylist-91956.appspot.com",
-  messagingSenderId: "813812426276",
-  appId: "1:813812426276:web:93e5897af12892ff78dab1",
-  measurementId: "G-VZ83BTR72T"
+  apiKey: 'AIzaSyAzcwgZuLA7dO9g4sQhXQVTUgCo0M8m2qM',
+  authDomain: 'grocerylist-91956.firebaseapp.com',
+  databaseURL: 'https://grocerylist-91956.firebaseio.com',
+  projectId: 'grocerylist-91956',
+  storageBucket: 'grocerylist-91956.appspot.com',
+  messagingSenderId: '813812426276',
+  appId: '1:813812426276:web:93e5897af12892ff78dab1',
+  measurementId: 'G-VZ83BTR72T',
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
@@ -16,18 +16,25 @@ testJson["lastname"] = "zhang";
 testJson["location"] = "aiken";
 console.log(testJson);
 */
+console.log('here');
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+    console.log(user.email);
+    // ...
+  } else {
+    // User is signed out
+    console.log('no user');
+  }
+});
 
 // enter data in
-$("input[type='button']").click(function(e) {
+$("input[type='button']").click(function (e) {
   //get the value of form
-  
-  
+
   /* save the data to database */
 
-
-
   /* clear the entry */
-  $("form")[0].reset();
+  $('form')[0].reset();
 });
 
 /* array example
@@ -36,15 +43,16 @@ array1.forEach(element => console.log(element));
 */
 
 /* read the data from the database */
-
+/*
 firebase
   .firestore()
-  .collection("hoteldata")
-  .onSnapshot(querySnapshot => {
+  .collection('hoteldata')
+  .onSnapshot((querySnapshot) => {
     console.log(querySnapshot.size);
-    querySnapshot.forEach(doc => {
+    querySnapshot.forEach((doc) => {
       console.log(doc.data());
       console.log(doc.data().room);
       console.log(doc.data().checkout);
     });
   });
+*/
